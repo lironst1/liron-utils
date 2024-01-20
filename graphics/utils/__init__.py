4f02b03a4d052a0c29c1f8dbcf008d40486a8162
lironst <lironst1@me.com>
2024-01-20 09:40:26 +0200

    ...

--- graphics/utils/__init__.py
+++ graphics/utils/__init__.py
@@ -1,16 +1,9 @@
-import matplotlib as mpl
 import matplotlib.style
 
 # matplotlib.style.use('seaborn')
 
 
-from .RC_PARAMS import rcParams, plot_color_cycler
+from .rc_params import *
 from . import COLORS
 
-
-print("Changing default MatPlotLib rcParams...")
-mpl.rcParams.update(rcParams)  # Change default MatPlotLib parameters (e.g, figure size, label size, grid, colors, etc.)
-
-
-def update_rcParams(new_rcParams: dict):
-    mpl.rcParams.update(new_rcParams)
+update_rcParams(RC_PARAMS)  # Change default MatPlotLib parameters (e.g, figure size, label size, grid, colors, etc.)
