bd62dee6ee04b58e4f03f1560059dec4ec4e088d
Liron Stettiner <lironst1@me.com>
2024-07-10 13:55:13 +0300

    ...

---
+++ MATLAB/utils/graphics/create_figs.m
@@ -0,0 +1,56 @@
+function figs = create_figs(fig_kwargs, shape)
+%create_figs creates figures with axes defined by <shape>.
+%
+% Arguments:
+%   - shape - a repeating input of  2-element arrays.
+%             <shape{i}>=[nrows, nclos] speciefies the subplots of the i-th
+%             figure
+% Returns:
+%   - figs  - a struct of length(shape) with handles to the figure and all
+%             its axes:
+%             figs(i):
+%               - fig
+%               - axs(j)
+% 
+% Example:
+%   >> figs = create_figs([2,1], [3,1]);
+%         figs(1)            figs(2)
+%       |=========|        |=========|
+%       | axs1(1) |        | axs2(1) |
+%       |=========|        | axs2(2) |
+%       | axs1(2) |        | axs2(3) |
+%       |=========|        |=========|
+% 
+arguments
+    fig_kwargs = {}
+end
+arguments (Repeating)
+    shape       % shape of subplots in the i-th figure
+end
+
+set(0, "DefaultFigureWindowStyle", "docked")
+
+if isempty(shape)
+    shape = {[1, 1]};
+end
+
+nFigures = length(shape);
+
+figs = struct();
+for i = 1 : nFigures
+    figs(i).fig = figure(fig_kwargs{:});
+
+    axsShape = shape{i};
+    h_tiled = tiledlayout(figs(i).fig, axsShape(1), axsShape(2), ...
+        "TileSpacing", "compact", ...
+        "Padding", "compact");
+    for j = 1 : prod(axsShape)
+        figs(i).axs(j) = nexttile(h_tiled, j);
+        cla(figs(i).axs(j))
+        hold(figs(i).axs(j), "on")
+    end
+end
+
+set(0, "DefaultFigureWindowStyle", "default")
+
+end
---
+++ MATLAB/utils/graphics/plot_ops.m
@@ -0,0 +1,86 @@
+function plot_ops(ax, options)
+arguments
+    ax
+    options.title           string  = []
+    options.xlabel          string  = []
+    options.ylabel          string  = []
+    options.zlabel          string  = []
+    options.grid    (1,1)   string  = "on"
+    options.axis            string  = []
+    options.shading         string  = []
+    options.xlim            double  = []
+    options.ylim            double  = []
+    options.zlim            double  = []
+    options.legend          logical = false
+    options.view            double = []
+
+    options.filename        string  = []
+    options.format  (1,1)   string  = ".png"
+    options.closefig        logical = false
+
+    options.title_kwargs    cell    = {"FontSize", 16, "Interpreter", "latex"}
+    options.label_kwargs    cell    = {"FontSize", 12, "Interpreter", "latex"}
+end
+
+if ax.Type == "axes"  % in case user wants to save figure, a figure handle can also be passed
+    if ~isempty(options.title)
+        title(ax, options.title, options.title_kwargs{:})
+    end
+    if ~isempty(options.xlabel)
+        xlabel(ax, options.xlabel, options.label_kwargs{:})
+    end
+    if ~isempty(options.ylabel)
+        ylabel(ax, options.ylabel, options.label_kwargs{:})
+    end
+    if ~isempty(options.zlabel)
+        zlabel(ax, options.zlabel, options.label_kwargs{:})
+    end
+    if ~isempty(options.grid)
+        grid(ax, options.grid)
+    end
+    if ~isempty(options.axis)
+        axis(ax, options.axis)
+    end
+    if ~isempty(options.shading)
+        shading(ax, options.shading)
+    end
+    if ~isempty(options.xlim)
+        xlim(ax, options.xlim)
+    end
+    if ~isempty(options.ylim)
+        ylim(ax, options.ylim)
+    end
+    if ~isempty(options.zlim)
+        zlim(ax, options.zlim)
+    end
+    if options.legend
+        legend(ax, options.label_kwargs{:})
+    end
+    if options.view
+        view(ax, options.view)
+    end
+end
+
+
+if ~isempty(options.filename)
+    fig = ancestor(ax, "figure");
+
+    [dirname, filename, format] = fileparts(options.filename);
+    dirname = fullfile(dirname, "figs");
+    if ~exist(dirname, "dir")
+        mkdir(dirname)
+    end
+    if strlength(format) > 0
+        options.format = format;
+    end
+    options.filename = fullfile(dirname, filename + options.format);
+
+    exportgraphics(fig, options.filename)
+
+    if options.closefig
+        close(fig)
+    end
+
+end
+
+end
