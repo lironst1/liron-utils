29b0c1103677af48f08a183a5f8f5e6cedd1dbc8
Liron Stettiner <lironst1@me.com>
2024-12-08 02:22:51 +0200

    ...

--- signal_processing/fitting.py
+++ signal_processing/fitting.py
@@ -2,9 +2,9 @@ import numpy as np
 import scipy.stats
 import scipy.optimize
 import scipy.odr as odr
-import uncertainties.core
+import scipy.signal
 from sklearn.metrics import r2_score
-from ..uncertainties_math import to_numpy, from_numpy, ufloat, val
+from ..uncertainties_math import ufloat, val, to_numpy, from_numpy
 from .. import graphics as gr
 
 linear_regression = scipy.stats.linregress
@@ -116,7 +116,7 @@ def curve_fit(fit_fcn,
 
 	x, xerr = to_numpy(x, xerr)
 	y, yerr = to_numpy(y, yerr)
-	p0, _ = to_numpy(p0)
+	p0 = val(p0)
 
 	if xerr is not None and xerr.size == 1:
 		xerr = np.repeat(xerr, len(x))
@@ -139,3 +139,75 @@ def curve_fit(fit_fcn,
 		return p_opt_err, p_cov, Ax
 
 	return p_opt_err, p_cov
+
+
+def find_peaks(
+		y, yerr=None,
+		x=None, xerr=None,
+		n_points=3,
+		plot=False,
+		**kwargs):
+	"""
+
+	Parameters
+	----------
+	x :
+	y :
+	xerr :
+	yerr :
+	kwargs :
+
+	Returns
+	-------
+
+	"""
+
+	x, xerr = to_numpy(x, xerr)
+	y, yerr = to_numpy(y, yerr)
+	if x is None:
+		x = np.arange(len(y))
+	if xerr is not None and xerr.size == 1:
+		xerr = np.repeat(xerr, len(x))
+	if yerr is not None and yerr.size == 1:
+		yerr = np.repeat(yerr, len(y))
+
+	peaks, properties = scipy.signal.find_peaks(val(y), **kwargs)
+
+	# fit quadratic around peaks
+	def quadratic(x, a, b, c):
+		return a * x ** 2 + b * x + c
+
+	x_peaks_fit = from_numpy(np.zeros(len(peaks)), 0)
+	y_peaks_fit = from_numpy(np.zeros(len(peaks)), 0)
+	for i in range(len(peaks)):
+		# Define a small range around the peak
+		left = max(0, peaks[i] - n_points)
+		right = min(len(y), peaks[i] + n_points + 1)
+
+		# Perform quadratic fit
+		x0 = np.mean(x[left:right])
+		popt, pcov = curve_fit(fit_fcn=quadratic,
+				x=x[left:right] - x0,
+				y=y[left:right],
+				xerr=xerr[left:right] if xerr is not None else None,
+				yerr=yerr[left:right] if yerr is not None else None,
+				p0=[0, 0, y[peaks[i]]],
+		)
+		a_center, b_center, c_center = popt
+		a = a_center
+		b = b_center - 2 * a_center * x0
+		c = a_center * x0 ** 2 - b_center * x0 + c_center
+
+		x_peaks_fit[i] = - b / (2 * a)  # Vertex of the parabola (peak location)
+		y_peaks_fit[i] = quadratic(x_peaks_fit[i], a, b, c)
+
+	if plot:
+		Ax = gr.AxesLiron()
+		Ax.plot_errorbar(x, y,
+				xerr=xerr, yerr=yerr)
+		Ax.plot_errorbar(x_peaks_fit, y_peaks_fit,
+				marker="o", color="black", linestyle="none")
+
+		return x_peaks_fit, y_peaks_fit, peaks, properties, Ax
+
+	return x_peaks_fit, y_peaks_fit, peaks, properties
