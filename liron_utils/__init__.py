36c1f541874d399bca01cc9bb816ffa7a1a0e178
Liron Stettiner <liron.stettiner@exodigo.ai>
2025-08-27 11:48:13 +0300

    Fix module structure to support pip install

---
+++ liron_utils/__init__.py
@@ -0,0 +1,26 @@
+"""Liron Utils - A collection of utility modules."""
+
+# Import all submodules
+from . import graphics
+from . import signal_processing
+from . import machine_learning
+from . import pure_python
+from . import symbolic_math
+from . import time
+from . import uncertainties_math
+from . import files
+from . import web
+from . import manim_animations
+
+__all__ = [
+    'graphics',
+    'signal_processing',
+    'machine_learning',
+    'pure_python',
+    'symbolic_math',
+    'time',
+    'uncertainties_math',
+    'files',
+    'web',
+    'manim_animations'
+]
