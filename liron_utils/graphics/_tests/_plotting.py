1a58d78ea361156e18b51046ade8565d60c1f5a3
Liron Stettiner <lironst1@me.com>
2025-09-05 18:15:33 +0300

    Add analyze_window and analyze_filter

--- liron_utils/graphics/_tests/_plotting.py
+++ liron_utils/graphics/_tests/_plotting.py
@@ -1,16 +1,22 @@
 import numpy as np
+from scipy.signal import firwin2, butter
+from scipy.signal import windows
+
 from liron_utils import graphics as gr
-from scipy.signal import firwin2, butter, filtfilt
+from liron_utils.signal_processing import analyze_window, analyze_filter
 
-gr.update_rcParams("liron-utils-text-color", "white")
-
-b = firwin2(numtaps=50, freq=[0, 0.19, 0.21, 1], gain=[1, 1, 0, 0])
+b = firwin2(numtaps=21, freq=[0, 0.1, 0.2, 1], gain=[1, 1, 0, 0])
+# b = windows.get_window("hamming", 6)
+# b /= b.sum()
 a = 1
 
-# b, a = butter(N=4, Wn=0.8)
+# b, a = butter(N=8, Wn=[0.3, 0.7], btype="bandpass")
 
-Ax = gr.Axes(shape=(3, 1))
-Ax[0, 0].plot_impulse_response(b, a, dt=1, n=500)
-Ax[1, 0].plot_frequency_response(b, a, one_sided=False, which="amp")
-Ax[2, 0].plot_frequency_response(b, a, one_sided=False, which="phase")
-Ax.set_props()
+# out = analyze_window(b, fs=None, plot=True)
+out = analyze_filter(b, a, plot=True)
+
+# Ax = gr.Axes(shape=(3, 1))
+# Ax[0, 0].plot_impulse_response(b, a, n=50)
+# Ax[1, 0].plot_frequency_response(b, a, one_sided=False, which="amp")
+# Ax[2, 0].plot_frequency_response(b, a, one_sided=False, which="phase")
+# Ax.set_props()
